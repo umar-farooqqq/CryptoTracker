a6272c6e6b18497deeb6add1847dbe326c24a3bd
umarfarooqqq <umarfarooq63425@gmail.com>
2025-07-09 19:50:22 +0500

    final

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -49,47 +49,47 @@ const Coin = () => {
 
   if (coinData && historicalData) {
     return (
-      <div className="">
-        <div>
-          <img src={coinData.image.large} alt="" />
-          <p>
+      <div className="coin p-[0_20px]">
+        <div className="coin-name flex flex-col items-center gap-[20px] m-[100px] mb-[50px]">
+          <img src={coinData.image.large} alt="" className="max-w-[100px]" />
+          <p className="text-4xl">
             <b className="text-white">
               {coinData.name} ({coinData.symbol.toUpperCase()})
             </b>
           </p>
         </div>
-        <div className="coinChart">
+        <div className="coinChart max-w-[600px] h-[250px] m-auto">
           <LineChart endpoint={historicalData} />
         </div>
 
-        <div className="coinDetails text-white">
-          <ul>
-            <li>Crypto Market Rank</li>
+        <div className="coinDetails text-white max-w-[600px] flex flex-col mx-auto mt-[40px] mb-[100px]">
+          <ul className="flex justify-between p-[10px_0px] border-b-[1px] border-[#5f5d5f] list-none">
+            <li className="font-mono text-lg">Crypto Market Rank</li>
             <li>{coinData.market_cap_rank}</li>
           </ul>
-          <ul>
-            <li>Current Price</li>
+          <ul className="flex justify-between p-[10px_0px] border-b-[1px] border-[#5f5d5f] list-none">
+            <li className="font-mono text-lg">Current Price</li>
             <li>
               {currency.symbol}{" "}
               {coinData.market_data.current_price?.[currency.name]?.toLocaleString() || "N/A"}
             </li>
           </ul>
-          <ul>
-            <li>Market Cap</li>
+          <ul className="flex justify-between p-[10px_0px] border-b-[1px] border-[#5f5d5f] list-none">
+            <li className="font-mono text-lg">Market Cap</li>
             <li>
               {currency.symbol}{" "}
               {coinData.market_data.market_cap?.[currency.name]?.toLocaleString() || "N/A"}
             </li>
           </ul>
-          <ul>
-            <li>24H High</li>
+          <ul className="flex justify-between p-[10px_0px] border-b-[1px] border-[#5f5d5f]  list-none">
+            <li className="font-mono text-lg">24H High</li>
             <li>
               {currency.symbol}{" "}
               {coinData.market_data.high_24h?.[currency.name]?.toLocaleString() || "N/A"}
             </li>
           </ul>
-          <ul>
-            <li>24H Low</li>
+          <ul className="flex justify-between p-[10px_0px] border-b-[1px] border-[#5f5d5f] list-none">
+            <li className="font-mono text-lg">24H Low</li>
             <li>
               {currency.symbol}{" "}
               {coinData.market_data.low_24h?.[currency.name]?.toLocaleString() || "N/A"}
